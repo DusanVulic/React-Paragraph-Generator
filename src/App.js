1f46ba58d07e1b9982b57bdf3fd9f72a5a0c5bc5
Dusan Vulic <dusanvulic11@gmail.com>
2021-07-22 23:01:17 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+import data from './data'
+
 
 function App() {
+
+
+const [text,setText]=useState([]);
+
+const [count,setCount]=useState(0);
+
+const [note,setNote]=useState(false)
+
+const handleSubmit=(event)=>{
+  event.preventDefault();
+  let amount= parseInt(count);
+  console.log('form is submitted',count,amount);
+
+  if(count<=0 || count>8){
+    setNote(true)
+  }
+if (count>0 && count<=8){
+  setText(data.slice(0,amount));
+  setNote(false);
+}
+  
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section-center">
+      <h3> tired of boring lorem ipsum ?</h3>
+      <p> please enter value between 1 and 8 to generate paragraphs</p>
+      <form className="lorem-form" onSubmit={handleSubmit}> 
+      <label htmlFor="amount" > paragraphs :</label>
+      <input  type="number" name="number" id="number" value={count} onChange={(event)=>setCount(event.target.value)}/>
+      <button type="submit" className="btn" >generate</button>
+
+      </form>
+
+      {note && <article className="lorem-text"> <p className="warning"> please enter value between 1 and 8 !</p></article>}
+
+      {!note  &&   
+      <article className="lorem-text">
+        
+        {text.map((item,index)=>( 
+          <p key={index}>{item}  </p>
+        ))}
+      </article>
+      }
+    </section>
   );
 }
 
